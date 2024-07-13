cccdda20631b0a2f463ab99a0471734ef6c657a4
wkauan <kauanmbomfim5@gmail.com>
2024-07-12 21:29:44 -0300

    feat: Implementando rota para participante

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -10,10 +10,13 @@ from src.controllers.trip_confirmer import TripConfirmer
 from src.controllers.link_creator import LinkCreator
 from src.controllers.link_finder import LinkFinder
 
+from src.controllers.participant_creator import ParticipantCreator
+
 # Importação de repositorios
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
 from src.models.repositories.links_repository import LinksRepository
+from src.models.repositories.participants_repository import ParticipantsRepository
 
 # Importação do gerente de conexões
 from src.models.settings.db_connection_handler import db_connection_handler
@@ -70,6 +73,17 @@ def find_trip_link(tripId):
     controller = LinkFinder(links_repository)
 
     response = controller.find(tripId)
+
+
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/participants/<participantId>/confirm'", methods=["POST"])
+def create_participant(tripId):
+    conn = db_connection_handler.get_connection()
+    participants_repository = ParticipantsRepository(conn)
+    controller = ParticipantCreator(participants_repository)
+
+    response = controller.create(request.json, tripId)
 
 
     return jsonify(response["body"]), response["status_code"]
